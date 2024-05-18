7117a65e916b368716d0f90c3f783838be690d84
Krish Nalam <nalamkrish1@gmail.com>
2024-05-18 03:29:04 -0400

    Log in page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         </Routes>
       </AuthProvider>
     </Router>
+    
   );
 }
 
--- FrontEnd/src/Components/Login.jsx
+++ FrontEnd/src/Components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import './LogInPage.css'
 
 const Login = () => {
   const [formData, setFormData] = useState({ username: '', password: '' });
@@ -16,8 +17,13 @@ const Login = () => {
 
   return (
     <form onSubmit={onSubmit}>
+      <h2>Login/SignUp</h2>
+      <div className="form-group">
+      <label htmlFor="email">Email:</label>
       <input type="text" name="username" value={username} onChange={onChange} required />
+      <label htmlFor="password">Password:</label>
       <input type="password" name="password" value={password} onChange={onChange} required />
+      </div>
       <button type="submit">Login</button>
     </form>
   );

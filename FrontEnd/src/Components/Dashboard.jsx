eec6de1162277d567b72e4816d22e997036e0175
Krish Nalam <nalamkrish1@gmail.com>
2024-05-18 16:06:25 -0400

    some more ui/ux stuff

--- FrontEnd/src/Components/Dashboard.jsx
+++ FrontEnd/src/Components/Dashboard.jsx
@@ -26,75 +26,90 @@ const Dashboard = ({ data }) => {
           <UserIcon />
         </div>
       </div>
-      <div id="dashboredGraph">
-        <div id="dashboredCompareDiv">
-          <div id="dashboredCompareTitleDiv">
-            <h1>{data.team1.name}</h1>
-            <h1>{data.team2.name}</h1>
-          </div>
-          <div id="dashboredComparePerDiv">
-            <div className="chanWinningTxt">
-              <h1 >{data.team1.chanceOfWinning}%</h1>
-              <h4>Chance Of Winning</h4>
+      <div id="navbarUnderline"></div> 
+      <div className = "leftthings">
+        <div id="dashboredGraph">
+          <div id="dashboredCompareDiv">
+            <div id="dashboredCompareTitleDiv">
+              <h1>{data.team1.name}</h1>
+              <h1>{data.team2.name}</h1>
             </div>
-            <h2>VS</h2>
-            <div className="chanWinningTxt">
-              <h1>{data.team2.chanceOfWinning}%</h1>
-              <h4>Chance Of Winning</h4>
+            <div id="dashboredComparePerDiv">
+              <div className="chanWinningTxt">
+                <h1 >{data.team1.chanceOfWinning}%</h1>
+                <h4>Chance Of Winning</h4>
+              </div>
+              <h2>VS</h2>
+              <div className="chanWinningTxt">
+                <h1>{data.team2.chanceOfWinning}%</h1>
+                <h4>Chance Of Winning</h4>
+              </div>
+            </div>
+          </div>
+          <div id="topBetters">
+            <div id="betterTitle">
+              <h1>Top Betters</h1>
+              <ul>
+                <li><a href="#">Neha Kasoju</a><a href="#">$800,000</a></li>
+                <li><a href="#">Neha Kasoju</a><a href="#">$800,000</a></li>
+                <li><a href="#">Neha Kasoju</a><a href="#">$800,000</a></li>
+                <li><a href="#">Neha Kasoju</a><a href="#">$800,000</a></li>
+              </ul>
             </div>
           </div>
         </div>
-      </div>
-      <div id="dashboredBet">
-        <button
-          className={`radio-button ${selectedOption === 500 ? "selected" : ""}`}
-          onClick={() => handleOptionChange(500)}
-        >
-          $500
-        </button>
-        <button
-          className={`radio-button ${selectedOption === 400 ? "selected" : ""}`}
-          onClick={() => handleOptionChange(400)}
-        >
-          $400
-        </button>
-        <button
-          className={`radio-button ${selectedOption === 7000 ? "selected" : ""}`}
-          onClick={() => handleOptionChange(7000)}
-        >
-          $7000
-        </button>
-        <button
-          className={`radio-button ${selectedOption === 9000 ? "selected" : ""}`}
-          onClick={() => handleOptionChange(9000)}
-        >
-          $9000
-        </button>
-        <button
-          className={`radio-button ${selectedOption === 6600 ? "selected" : ""}`}
-          onClick={() => handleOptionChange(6600)}
-        >
-          $6600
-        </button>
-        <button
-          className={`radio-button ${selectedOption === 200 ? "selected" : ""}`}
-          onClick={() => handleOptionChange(200)}
-        >
-          $200
-        </button>
-
-        <div id="dashboredBetRight">
+        <div id="dashboredBet">
           <button
-            className={`radio-button ${selectedOption === "custom" ? "selected" : ""}`}
-            onClick={() => handleOptionChange("custom")}
+            className={`radio-button ${selectedOption === 500 ? "selected" : ""}`}
+            onClick={() => handleOptionChange(500)}
           >
-            custom
+            $500
           </button>
-          <button className="radio-button" id="betBtn">
-            Bet
+          <button
+            className={`radio-button ${selectedOption === 400 ? "selected" : ""}`}
+            onClick={() => handleOptionChange(400)}
+          >
+            $400
           </button>
+          <button
+            className={`radio-button ${selectedOption === 7000 ? "selected" : ""}`}
+            onClick={() => handleOptionChange(7000)}
+          >
+            $7000
+          </button>
+          <button
+            className={`radio-button ${selectedOption === 9000 ? "selected" : ""}`}
+            onClick={() => handleOptionChange(9000)}
+          >
+            $9000
+          </button>
+          <button
+            className={`radio-button ${selectedOption === 6600 ? "selected" : ""}`}
+            onClick={() => handleOptionChange(6600)}
+          >
+            $6600
+          </button>
+          <button
+            className={`radio-button ${selectedOption === 200 ? "selected" : ""}`}
+            onClick={() => handleOptionChange(200)}
+          >
+            $200
+          </button>
+
+          <div id="dashboredBetRight">
+            <button
+              className={`radio-button ${selectedOption === "custom" ? "selected" : ""}`}
+              onClick={() => handleOptionChange("custom")}
+            >
+              custom
+            </button>
+            <button className="radio-button" id="betBtn">
+              Bet
+            </button>
+          </div>
         </div>
       </div>
+
     </div>
   );
 };

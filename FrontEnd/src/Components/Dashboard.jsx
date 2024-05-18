729eb62919b1cdafa8d39e04b675befd751540ca
Sidak <magicalsidak@gmail.com>
2024-05-18 16:53:41 -0400

    idk

--- FrontEnd/src/Components/Dashboard.jsx
+++ FrontEnd/src/Components/Dashboard.jsx
@@ -17,17 +17,25 @@ const Dashboard = ({ data }) => {
       <div id="dashboredNavbar">
         <Logo />
         <div id="navbarLinks">
-          <a href="#" className="nav-link">Sports</a>
-          <a href="#" className="nav-link">Stocks</a>
-          <a href="#" className="nav-link">Politics</a>
-          <a href="#" className="nav-link">Enterainment</a>
+          <a href="#" className="nav-link">
+            Sports
+          </a>
+          <a href="#" className="nav-link">
+            Stocks
+          </a>
+          <a href="#" className="nav-link">
+            Politics
+          </a>
+          <a href="#" className="nav-link">
+            Enterainment
+          </a>
         </div>
         <div id="navbarUserIcon">
           <UserIcon />
         </div>
       </div>
-      <div id="navbarUnderline"></div> 
-      <div className = "leftthings">
+      <div id="navbarUnderline"></div>
+      <div className="leftthings">
         <div id="dashboredGraph">
           <div id="dashboredCompareDiv">
             <div id="dashboredCompareTitleDiv">
@@ -36,7 +44,7 @@ const Dashboard = ({ data }) => {
             </div>
             <div id="dashboredComparePerDiv">
               <div className="chanWinningTxt">
-                <h1 >{data.team1.chanceOfWinning}%</h1>
+                <h1>{data.team1.chanceOfWinning}%</h1>
                 <h4>Chance Of Winning</h4>
               </div>
               <h2>VS</h2>
@@ -49,12 +57,6 @@ const Dashboard = ({ data }) => {
           <div id="topBetters">
             <div id="betterTitle">
               <h1>Top Betters</h1>
-              <ul>
-                <li><a href="#">Neha Kasoju</a><a href="#">$800,000</a></li>
-                <li><a href="#">Neha Kasoju</a><a href="#">$800,000</a></li>
-                <li><a href="#">Neha Kasoju</a><a href="#">$800,000</a></li>
-                <li><a href="#">Neha Kasoju</a><a href="#">$800,000</a></li>
-              </ul>
             </div>
           </div>
         </div>
@@ -109,7 +111,6 @@ const Dashboard = ({ data }) => {
           </div>
         </div>
       </div>
-
     </div>
   );
 };
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://aloisekua:v6TIn4vCCikhTDoG@cluster0.t4bwm.mongodb.net/yourdatabase?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(
+      "mongodb+srv://aloisekua:v6TIn4vCCikhTDoG@cluster0.t4bwm.mongodb.net/yourdatabase?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    );
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
+
+//neurelo_9wKFBp874Z5xFw6ZCfvhXcs7m/qx/0Lte4zBlOC3euCBGHpb4oOzZDLpjfxXEQPpdhpsZfD3xLyYOWPARVPx8OZjOhGZjI5MdpkQdfsxE97oDhPzGh4TODzJXUWcbNBxnQW+S6QvK2xHJYBl2YT2OFF2ya62h+8EgyNw7VvGiyRyjtywpie9J7B2JUUVjfzI_7xigvAn5FvoSD/Kje1G0yHWq2YJQwYr6MkKtPYqbLOE=

c92db899c9a0b72d23130c54ac2fa215afffa74d
Krish Nalam <nalamkrish1@gmail.com>
2024-05-18 07:54:36 -0400

    better log in page

--- FrontEnd/src/Components/Login.jsx
+++ FrontEnd/src/Components/Login.jsx
@@ -32,28 +32,30 @@ const Login = ({ data, setData }) => {
   }, []);
 
   return (
-    <form onSubmit={onSubmit}>
-      <h2>Login/SignUp</h2>
-      <div className="form-group">
-        <label htmlFor="email">Email:</label>
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={onChange}
-          required
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={onChange}
-          required
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+    <div className="login-container">
+      <form onSubmit={onSubmit} className="login-form">
+        <h2>Login/SignUp</h2>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={onChange}
+            required
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 };
 

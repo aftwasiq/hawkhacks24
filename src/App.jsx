cff1cbee1d7fb034c175490615449de1bc010e5c
SyTaxus - NourELM <60050784+sytaxus@users.noreply.github.com>
2024-05-17 22:56:45 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Our project.</p>
+      </div>
+    </>
+  )
+}
+
+export default App

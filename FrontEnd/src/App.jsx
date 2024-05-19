05f723267d4856201c92fa0f4809fa6c1851a640
Sidak <magicalsidak@gmail.com>
2024-05-18 21:27:23 -0400

    the dashbored renders with near

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./Components/Register";
 import Login from "./Components/Login";
 import Dashboard from "./Components/Dashboard";
-import AuthProvider from './context/AuthProvider';
-import PrivateRoute from './context/PrivateRoute';
+import AuthProvider from "./context/AuthProvider";
+import PrivateRoute from "./context/PrivateRoute";
 import { useState } from "react";
 
 function App() {
@@ -51,9 +51,17 @@ function App() {
     <Router>
       <AuthProvider>
         <Routes>
-          <Route path="/login" element={<Login data={data} setData={setData} />} />
+          <Route
+            path="/login"
+            element={<Login data={data} setData={setData} />}
+          />
           <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={ <PrivateRoute><Dashboard data={data} /></PrivateRoute>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard data={data} />
+              </PrivateRoute>
             }
           />
         </Routes>
